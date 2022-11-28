10af9183edb1d34fca81e758b0ac37e83287f977
JotaCe7 <jcisneros@meicaperu.com>
2022-11-27 21:11:44 -0500

    add option to save and load objects using pickle library

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@
 import os
 from urllib.request import urlopen
 import tarfile
+import pickle
+
+PICKLE_PATH = 'data/'
+RAW_DATA_PICKLE = 'raw_data.pickle'
+NORM_DATA_PICKLE = 'normalized_data.pickle'
+
 
 def walkdir(folder):
     """
@@ -22,6 +28,18 @@ def walkdir(folder):
             yield (dirpath, filename)
 
 
+def save_object(object, object_name, data_path=PICKLE_PATH):
+  with open(os.path.join(data_path, object_name), 'wb') as f:
+    pickle.dump(object, f, protocol=pickle.HIGHEST_PROTOCOL)
+  return object
+
+def load_object(object_name, data_path=PICKLE_PATH):
+  with open(os.path.join(data_path, object_name), 'rb') as f:
+    object = pickle.load(f)
+  return object
+
+
+
 def donwload_data(url = 'https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'):
   with urlopen(url) as tgz:
       with tarfile.open(fileobj=tgz, mode="r|gz") as tgz:
@@ -29,7 +47,6 @@ def donwload_data(url = 'https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v
             print(file.name)
             if file.isfile() & (('/neg' in file.name) or ('/pos' in file.name)):
                 tgz.extract(file)
-
 
 def get_data(data_path):
   result = []
@@ -43,4 +60,12 @@ def get_data(data_path):
         y.append(target_dict[target])
     result.extend([X,y])
   return result
+
+def load_data(data_path):
+  if os.path.isfile(os.path.join(PICKLE_PATH, RAW_DATA_PICKLE)):
+    return load_object(RAW_DATA_PICKLE)
+  if not os.path.isdir(data_path):
+    donwload_data()
+  return save_object(get_data(data_path), RAW_DATA_PICKLE)
+
 X_train, y_train, X_test, y_test = get_data('data/')
