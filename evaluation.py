32e03950bbd289551a30448ec0aec42a2522d0a9
JotaCe7 <jcisneros@meicaperu.com>
2022-11-28 19:24:26 -0500

    add train and test roc plots as subplots

--- evaluation.py
+++ evaluation.py
@@ -33,21 +33,41 @@ def get_performance(predictions, y_test, labels=[1, 0]):
     return accuracy, precision, recall, f1_score
 
 
-def plot_roc(model, y_test, features):
+def plot_roc(model, y_test, features, ax=None, title='Receiver Operating Characteristic (ROC) Curve'):
     # Put your code
     y_proba = model.predict_proba(features)[:,1]
     fpr, tpr, _ = metrics.roc_curve(y_test, y_proba)
     roc_auc = metrics.roc_auc_score(y_test, y_proba)
 
-    plt.figure(figsize=(10, 5))
-    plt.plot(fpr, tpr, label=f'ROC curve (area = {roc_auc})', linewidth=2.5)
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
-    plt.legend(loc="lower right")
-    plt.show()
+    if ax is None:
+      plt.figure(figsize=(10, 5))
+      ax = plt
+      ax.xlim([0.0, 1.0])
+      ax.ylim([0.0, 1.05])
+      ax.xlabel('False Positive Rate')
+      ax.ylabel('True Positive Rate')
+      ax.title(title)
+      ax.legend(loc="lower right")
+    else:
+      ax.set_xlim([0.0, 1.0])
+      ax.set_ylim([0.0, 1.05])
+      ax.set_xlabel('False Positive Rate')
+      ax.set_ylabel('True Positive Rate')
+      ax.set_title(title)
 
+
+    ax.plot(fpr, tpr, label=f'ROC curve (area = {roc_auc})', linewidth=2.5)
+    ax.plot([0, 1], [0, 1], 'k--')
+    ax.legend(loc="lower right")
+    # ax.show()
+
+    return roc_auc
+
+def plot_roc_train_test(model, features_train, y_train, features_test, y_test, sup_title=""):
+
+    roc_auc = []
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20,5))
+    plt.suptitle(sup_title)
+    roc_auc.append(plot_roc(model, y_train, features_train, axes[0], title="Receiver Operating Characteristic (ROC) Curve in Train Set"))
+    roc_auc.append(plot_roc(model, y_test, features_test, axes[1], title="Receiver Operating Characteristic (ROC) Curve in Test Set"))
     return roc_auc
